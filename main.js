1545924ad0b6288156c46d6366c2678375e1cc8d
telmenb <tbayar2@illinois.edu>
2022-01-18 22:06:12 -0600

    styling and button functionality

--- main.js
+++ main.js
@@ -1,10 +1,44 @@
-const x = 10;
-
-grid = document.querySelector(".grid-container");
-grid.style.cssText = `grid-template-columns: repeat(${x}, ${32/x}rem); grid-template-rows: repeat(${x}, ${32/x}rem)`;
-
-for (let i = 0; i < x*x; i++) {
-    row = document.createElement("div");
-    row.style.cssText = `height: ${32/x}rem; width: ${32/x}rem; border: 0.01rem solid black`;
-    grid.append(row);
+function gridInit(x) {
+    //grid init
+    grid = document.querySelector(".grid-container");
+    grid.style.cssText = `grid-template-columns: repeat(${x}, ${40/x}rem)`;
+    
+    //squares init
+    for (let i = 0; i < x*x; i++) {
+        square = document.createElement("div");
+        square.classList = "element";
+        square.style.cssText = `height: ${40/x}rem; width: ${40/x}rem`;
+        grid.append(square);
+    }
 }
+function addHover() {
+    //hover event
+    elements = document.querySelectorAll(".element");
+    elements.forEach(element => {
+        element.addEventListener("mouseenter", event => {
+            event.target.classList.add("painted");
+        });
+    });
+}
+
+//dimensions
+let num = 16;
+gridInit(num);
+addHover();
+
+//clear button
+clear_button = document.querySelector("#clear-btn");
+clear_button.addEventListener("click", () => elements.forEach(element => element.classList.remove("painted")));
+
+//reset button
+reset_button = document.querySelector("#reset-btn");
+reset_button.addEventListener("click", () => {
+    elements.forEach(element => element.remove());
+    //TODO: sanitize user input-------------------------------------------------------------
+    const x = parseInt(prompt("Enter new integer dimensions (0 < x < 101):", `${num}`), 10);
+    num = x
+    //--------------------------------------------------------------------------------------
+    gridInit(x);
+    addHover();
+});
+
